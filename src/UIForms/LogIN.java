c169dd4ec3a37b42a5495c96483dafdf679e56c0
Adel Amer <Adel Amer@AdelAmer-PC>
2019-01-08 18:44:58 +0200

    add billing data class

--- src/UIForms/LogIN.java
+++ src/UIForms/LogIN.java
@@ -27,15 +27,15 @@ public class LogIN extends javax.swing.JFrame {
 
     public void search(String name, String Pass) {
         try {
-            IC.pst = IC.dbc.conn.prepareStatement("select userName, userPass from rest_cafe.user");
+            IC.pst = IC.dbc.conn.prepareStatement("select userName, userPass, from user");
             IC.rs = IC.pst.executeQuery();
             String n;
             String p;
             while (IC.rs.next()) {
                 n = IC.rs.getString("userName");
                 p = IC.rs.getString("userPass");
+                IC.jobTitle=IC.rs.getString("userType");
                 if (name.equals(n) && Pass.equals(p)) {
-
                     status = true;
                     break;
                 }
